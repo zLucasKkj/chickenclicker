dca20fda3fcb1e9ce5f23cedccf49a333137f2a8
Lucas <p.lucasgbcorreia@gmail.com>
2026-01-22 19:29:18 -0300

    Poor coded upd, fix soon.

--- script/main.js
+++ script/main.js
@@ -2,10 +2,10 @@
 function pickRandomArrayValue(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
-var game = {
-    unlocks: ["autoclicker"],
+var Game = {
+    unlocks: [],
     balance: 0,
-    cps: 0.25
+    cps: 5
 };
 window.oncontextmenu = function (event) {
     event.preventDefault();
@@ -14,24 +14,47 @@ window.oncontextmenu = function (event) {
 };
 window.onload = (e) => {
     const chickSounds = document.querySelectorAll('.ChickSound');
+    const errorSound = document.querySelector("#errorSound");
+    const boughtSound = document.querySelector("#boughtSound");
     const chick = document.querySelector('#chick');
     const cpsElement = document.querySelector("#cps");
     const chirpsElement = document.querySelector("#chirps");
+    const unlockableAutoclickerElement = document.querySelector("#unlockableAutoclicker img");
     function chirp(increment) {
-        cpsElement.innerHTML = `C$${game.cps} p/s`;
-        game.balance = game.balance + increment;
-        chirpsElement.innerHTML = game.balance.toFixed(2);
+        if (Game.unlocks.includes('autoclicker'))
+            cpsElement.innerHTML = Game.cps.toString();
+        Game.balance = Game.balance + increment;
+        chirpsElement.innerHTML = Game.balance.toFixed(2);
         new Audio(pickRandomArrayValue(chickSounds).src).play();
-        chick.animate({ transform: 'TranslateY(-5px)' }, { duration: 50 });
+        //chick.animate({ transform: 'TranslateY(-5px)' }, { duration: 50 });
     }
-    window.addEventListener('click', (element) => {
-        if (element.target === chick) {
+    window.addEventListener('mousedown', e => {
+        if (e.target === chick) {
+            chick.style = "transform: TranslateY(-5px);";
+        }
+    });
+    window.addEventListener('mouseup', e => {
+        if (e.target === chick) {
+            chick.style = "transform: TranslateY(5px);";
+        }
+    });
+    window.addEventListener('click', (e) => {
+        //document.documentElement.requestFullscreen()
+        if (e.target === chick) {
             chirp(1);
+        }
+        if (e.target === unlockableAutoclickerElement) {
+            if (Game.balance < 150)
+                return new Audio(errorSound.src).play();
+            Game.balance -= 150;
+            new Audio(boughtSound.src).play();
+            Game.unlocks.push("autoclicker");
+            document.querySelector("#unlockableAutoclicker").hidden = true;
         }
     });
     function step() {
-        if (game.unlocks.includes("autoclicker"))
-            chirp(game.cps);
+        if (Game.unlocks.includes("autoclicker"))
+            chirp(Game.cps);
         setTimeout(() => {
             step();
         }, 1000);
--- src/main.ts
+++ src/main.ts
@@ -8,41 +8,66 @@ interface Game {
     cps: number
 }
 
-var game: Game = {
-    unlocks: ["autoclicker"],
+var Game: Game = {
+    unlocks: [],
     balance: 0,
-    cps: 0.25
+    cps: 5
 }
 
-window.oncontextmenu = function(event) {
-     event.preventDefault();
-     event.stopPropagation();
-     return false;
+window.oncontextmenu = function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
 };
 
 window.onload = (e) => {
     const chickSounds = document.querySelectorAll('.ChickSound')!
-    const chick = document.querySelector('#chick')!
+    const errorSound = document.querySelector("#errorSound") as HTMLAudioElement
+    const boughtSound = document.querySelector("#boughtSound") as HTMLAudioElement
+
+    const chick = document.querySelector('#chick') as HTMLInputElement
 
     const cpsElement = document.querySelector("#cps")!
     const chirpsElement = document.querySelector("#chirps")!
 
-    function chirp(increment) {
-        cpsElement.innerHTML = `C$${game.cps} p/s`
-        game.balance = game.balance + increment
-        chirpsElement.innerHTML = game.balance.toFixed(2)
+    const unlockableAutoclickerElement = document.querySelector("#unlockableAutoclicker img") 
+
+    function chirp(increment: number) {
+        if(Game.unlocks.includes('autoclicker')) cpsElement.innerHTML = Game.cps.toString()
+        Game.balance = Game.balance + increment
+        chirpsElement.innerHTML = Game.balance.toFixed(2)
         new Audio(pickRandomArrayValue(chickSounds as any).src).play()
-        chick.animate({ transform: 'TranslateY(-5px)' }, { duration: 50 });
+        //chick.animate({ transform: 'TranslateY(-5px)' }, { duration: 50 });
     }
 
-    window.addEventListener('click', (element) => {
-        if (element.target === chick) {
-            chirp(1)      
+    window.addEventListener('mousedown', e => {
+        if (e.target === chick) {
+            chick.style = "transform: TranslateY(-5px);"
+        }
+    })
+
+    window.addEventListener('mouseup', e => {
+        if (e.target === chick) {
+            chick.style = "transform: TranslateY(5px);"
+        }
+    })
+
+    window.addEventListener('click', (e) => {
+        //document.documentElement.requestFullscreen()
+        if (e.target === chick) {
+            chirp(1)
+        }
+        if (e.target === unlockableAutoclickerElement) {
+            if (Game.balance < 150) return new Audio(errorSound.src).play()
+            Game.balance -= 150
+            new Audio(boughtSound.src).play()
+            Game.unlocks.push("autoclicker");
+            (document.querySelector("#unlockableAutoclicker") as HTMLDivElement).hidden = true
         }
     })
 
     function step() {
-        if (game.unlocks.includes("autoclicker")) chirp(game.cps)
+        if (Game.unlocks.includes("autoclicker")) chirp(Game.cps)
         setTimeout(() => {
             step()
         }, 1000)
